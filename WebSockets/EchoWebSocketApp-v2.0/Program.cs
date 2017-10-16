92e6bbc6559233e311825bc4539c596b3bcc1381
cperras <cperras3@gmail.com>
2017-10-16 00:33:40 -0700

    added C# asp.net core websocket echo services (1.1, 2.0)

---
+++ WebSockets/EchoWebSocketApp-v2.0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace EchoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(opts =>
+                {
+                    opts.Limits.MaxConcurrentConnections = null;
+                    opts.Limits.MaxConcurrentUpgradedConnections = null;
+                    // opts.Limits.KeepAliveTimeout
+                    // opts.ApplicationSchedulingMode
+
+                    // opts.Limits.KeepAliveTimeout
+                    // opts.
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                // .UseIISIntegration()
+                // .UseApplicationInsights()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
